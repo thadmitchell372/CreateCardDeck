3df366feb9dd1a68f367a73cf40474b7979515b3
thadmitchell372 <73081925+thadmitchell372@users.noreply.github.com>
2021-05-17 13:45:56 -0400

    Add files via upload

---
+++ Deck.js
@@ -0,0 +1,17 @@
+class Deck {
+    constructor() {
+      this.deck = [];
+  
+      const suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds'];
+      const values = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
+  
+      for (let suit in suits) {
+        for (let value in values) {
+          this.deck.push(`${values[value]} of ${suits[suit]}`);
+        }
+      }
+    }
+  }
+  
+  const deck1 = new Deck();
+  console.log(deck1.deck);
